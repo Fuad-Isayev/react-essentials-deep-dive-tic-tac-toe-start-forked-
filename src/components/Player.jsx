ae4f7341998dbc670a5ee4e2032eb69eb80e63a0
Fuad Isayev <105857316+Fuad-Isayev@users.noreply.github.com>
2023-11-14 08:25:58 +0000

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,13 +3,28 @@ import React from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsediting] = React.useState(false);
   const [playerName, setPlayerName] = React.useState(name);
+
+  const toggleIsEditing = () => {
+    setIsediting((prev) => !prev);
+  };
+
+  const handleNameChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let playerNameHTML = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    playerNameHTML = (
+      <input type="text" onChange={handleNameChange} value={playerName} />
+    );
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{playerName}</span>
+        {playerNameHTML}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={toggleIsEditing}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
